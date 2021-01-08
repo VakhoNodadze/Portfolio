5be3a18174c0e0523cb6291c3fba80fd2df4cadd
VakhoNodadze <vaxonodadze@gmail.com>
2021-01-08 19:01:13 +0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0a1e14eec502d3bf227c.js"
+    "url": "webpack-runtime-0c3a6e5b755570cd05bf.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "11b7756719577ac7c9bf86ecb55a71fe"
+    "revision": "7ba77a84ee0b977826c470553143b8f8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "7adaa2c76708427f67bbf9c296f790ee"
+    "revision": "084ec29abd8e12251e1478d54f24d7df"
   },
   {
     "url": "polyfill-ca8adf9770cfeec4c092.js"
